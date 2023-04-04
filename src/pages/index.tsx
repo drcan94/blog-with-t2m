f3af7dd06130bf63b852d022a3225c9324862fde
Burak CAN <drcan94@gmail.com>
2023-04-05 01:52:10 +0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Create T3 App</title>
+      </Head>
+      
+    </>
+  );
+};
+
+export default Home;
